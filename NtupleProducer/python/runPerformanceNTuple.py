b2b0f51a30d2261efea9fd8e9f9cf50a93435eb6
schaefes <86061473+schaefes@users.noreply.github.com>
2026-05-18 16:40:51 +0200

    Add error handling for l1tSC4NGJetModelPath

--- NtupleProducer/python/runPerformanceNTuple.py
+++ NtupleProducer/python/runPerformanceNTuple.py
@@ -334,7 +334,10 @@ def addNNPuppiTaus():
 
 def addMultitagging(): #extended TRK
     classes = process.l1tSC4NGJetProducer.classes
-    process.l1tSC4NGJetProducer.l1tSC4NGJetModelPath = cms.string(os.environ['CMSSW_BASE']+"/src/L1TSC4NGJetModel/L1TSC4NGJetModel_PtPU1")
+    try:
+        process.l1tSC4NGJetProducer.l1tSC4NGJetModelPath = cms.string(os.environ['CMSSW_BASE']+"/src/L1TSC4NGJetModel/L1TSC4NGJetModel_PtPU1")
+    except:
+        process.l1tSC4NGJetProducer.l1tSC4NGJetModelPath = cms.string(os.environ['CMSSW_BASE']+"/src/L1TSC4NGJetModel/L1TSC4NGJetModel_PtPU1/L1TSC4NGJetModel_PtPU1")
     for i in range(8):
         setattr(process.l1pfjetTaggerTable.moreVariables, "tagScore_%s" % (classes[i]), cms.string("getTagScores()[%s]"  % (i)))
 
